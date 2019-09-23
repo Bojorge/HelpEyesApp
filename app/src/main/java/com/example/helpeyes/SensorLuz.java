46eb0282493749b8fa7e7399e05becb511d046cf
Bojorge <duendenener@gmail.com>
2019-09-23 11:54:04 -0600

    El sensor de luz envia los datos al server
    desde el app se pueden enviar comandos para hacer pruebas

--- app/src/main/java/com/example/helpeyes/SensorLuz.java
+++ app/src/main/java/com/example/helpeyes/SensorLuz.java
@@ -11,13 +11,19 @@ import android.widget.Toast;
 
 import static android.hardware.Sensor.TYPE_LIGHT;
 
-public class SensorLuz extends Activity {
+public class SensorLuz extends Activity implements SensorEventListener{
 
     TextView intensidadLuz;
+
+
+
+    String intensidad;
 
     SensorManager sm;
     Sensor sensorLuz;
     SensorEventListener lightEventListener;
+
+    Cliente c=new Cliente(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,39 +32,84 @@ public class SensorLuz extends Activity {
 
         intensidadLuz=(TextView)findViewById(R.id.txtLuz);
 
-        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensorLuz =sm.getDefaultSensor(TYPE_LIGHT);
+        //sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        //sensorLuz =sm.getDefaultSensor(TYPE_LIGHT);
 
+        /*
         if(sensorLuz==null){
             Toast.makeText(this,"no hay sensor de luz",Toast.LENGTH_SHORT).show();
             finish();
         }
-
+        */
+        /*
         lightEventListener=new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent sensorEvent) {
-                float intensidad=sensorEvent.values[0];
-                intensidadLuz.setText("intensidad de luz:  "+intensidad);
+
+
+                float intensi=sensorEvent.values[0];
+                intensidadLuz.setText("intensidad de luz:  "+intensi);
+
+
             }
+
 
             @Override
             public void onAccuracyChanged(Sensor sensor, int i) {
 
             }
         };
+        */
+
+
     }
+
 
     @Override
     protected void onPause() {
         super.onPause();
-        sm.unregisterListener(lightEventListener);
+
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        sm.unregisterListener(this, sensorLuz);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        sm.registerListener(lightEventListener, sensorLuz,sm.SENSOR_DELAY_FASTEST);
+
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorLuz =sm.getDefaultSensor(TYPE_LIGHT);
+        if(sensorLuz==null){
+            Toast.makeText(this,"no hay sensor de luz",Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        sm.registerListener(this, sensorLuz,sm.SENSOR_DELAY_FASTEST);
     }
 
 
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Cliente c=new Cliente(this);
+
+        float intensidad=sensorEvent.values[0];
+        intensidadLuz.setText("intensidad de luz:  "+intensidad);
+
+        if(intensidad<20){
+            c.execute("dark");
+        }
+        else if(intensidad<100 && intensidad>20){
+            c.execute("black");
+        }
+        else if(intensidad>500){
+            c.execute("light");
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
 }
